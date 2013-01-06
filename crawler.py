1c13f729758216255129de954df5d1d03aa720f7
iramari <iramari@server.fake>
2013-01-06 14:31:01 -0600

    Added crawler.py interface. Extended simplespider.py to support more complex rules for crawling sites.

---
+++ crawler.py
@@ -0,0 +1,33 @@
+"""
+Provides a simple interface to command the simplespider Scrapy spider.
+"""
+import argparse
+
+import scrapy.cmdline
+
+parser = argparse.ArgumentParser(description='Command-line Scrapy crawler')
+parser.add_argument('-u', '--url', dest='urls', help='Comma-separated list of urls to crawl.')
+parser.add_argument('-m', '--mirror', action='store_true', help="Crawl all internal anchor links on the first url's domain.")
+parser.add_argument('-a', '--accept', dest='reg_accept',
+                    help='Crawl only anchor links which match supplied comma-separated regular expressions.')
+parser.add_argument('-x', '--reject', dest='reg_reject',
+                    help='Do not crawl anchor links which match supplied comma-separated regular expressions.')
+parser.add_argument('-D', '--domains',
+                    help='Crawl only anchor links on the specified domains. Overrides -a.')
+parser.add_argument('--url-file', help='File containing a list of urls to crawl.')
+args = parser.parse_args()
+
+cmds = ['scrapy', 'crawl', 'simplespider']
+
+if args.urls is not None:
+    cmds.extend(['-a', 'urls=%s' % args.urls])
+if args.url_file is not None:
+    cmds.extend(['-a', 'url-file=%s' % args.url_file])
+if args.reg_accept is not None:
+    cmds.extend(['-a', 'reg_accept=%s' % args.reg_accept])
+if args.reg_reject is not None:
+    cmds.extend(['-a', 'reg_reject=%s' % args.reg_reject])
+if args.mirror:
+    cmds.extend(['-a', 'mirror=true'])
+
+scrapy.cmdline.execute(cmds)
--- crawltest/spiders/simplespider.py
+++ crawltest/spiders/simplespider.py
@@ -5,6 +5,7 @@ import xml.etree.ElementTree
 from scrapy.spider import BaseSpider
 from scrapy.selector import HtmlXPathSelector
 from scrapy.http import Request
+from scrapy import log
 
 class SimpleSpider(BaseSpider):
     name = "simplespider"
@@ -13,6 +14,7 @@ class SimpleSpider(BaseSpider):
     def load_sitemap(sitemap):
         tree = xml.etree.ElementTree.parse(sitemap)
         root = tree.getroot()
+        self.regs_accept = None
 
         found_urls = []
         for url in root:
@@ -22,14 +24,81 @@ class SimpleSpider(BaseSpider):
                 found_urls.append(loc.text)
         return found_urls
 
-    def __init__(self, url=None, sitemap=None):
+    def __init__(self, urls=None, sitemap=None, mirror=None, reg_accept=None,
+                 reg_reject=None, domains=None):
         self.start_urls = []
-        if url is not None:
-            self.start_urls.append(url)
+        self.accept_netlocs = [] # By default, do not crawl links
+        self.regs_accept = None
+        self.regs_reject = None
+        self.mirror = False
+
+        if urls is not None:
+            urls = urls.split(',')
+            self.start_urls.extend(urls)
+            if mirror is not None:
+                parts = urlparse.urlparse(urls[0])
+                self.accept_netlocs = [parts.netloc.lower()]
         if sitemap is not None:
+            self.log("Sitemap crawl: %s" % sitemap, log.DEBUG)
             urls = self.load_sitemap(sitemap)
             self.start_urls.extend(urls)
+        if reg_accept is not None:
+            regs = reg_accept.split(',')
+            self.regs_accept = [re.compile(reg) for reg in regs]
+            self.accept_netlocs = None # ignore netlocs, go based on regexp only
+                                       # unless domains is specified to override
+        if reg_reject is not None:
+            regs = reg_reject.split(',')
+            self.regs_reject = [re.compile(reg) for reg in regs]
+        if domains is not None:
+            domains = domains.split(',')
+            self.accept_netlocs = domains
 
+        if self.accept_netlocs:
+            self.accept_netlocs = [n.lower() for n in self.accept_netlocs]
+            self.log("Using accept_netlocs: %s" %self.accept_netlocs, log.DEBUG)
+        if len(self.start_urls) > 5:
+            self.log("Crawling many urls: %d" % len(self.start_urls), log.DEBUG)
+        else:
+            self.log("Crawling start_urls: %s" % self.start_urls, log.DEBUG)
+
+    """
+    Pure function that applies a set of rules to a url to determine if it should
+    be crawled. Returns True if it should be crawled.
+
+    """
+    @staticmethod
+    def crawl_ruled(url, accept_netlocs=None, regs_accept=None,
+                    regs_reject=None):
+        # If None is supplied to netlocs, ignore accept_netlocs
+        # If an empty list [] is supplied, returns False
+        if accept_netlocs is not None:
+            parts = urlparse.urlparse(url)
+            if parts.netloc.lower() not in accept_netlocs:
+                return False
+
+        if regs_accept is not None:
+            accept = False
+            for comp in regs_accept:
+                if comp.search(url):
+                    accept = True
+            if accept is False:
+                return False
+
+        if regs_reject is not None:
+            accept = True
+            for comp in regs_reject:
+                if comp.search(url):
+                    accept = False
+            if accept is False:
+                return False
+
+        return True
+
+    """
+    Crawls a response containing a CSS file
+
+    """
     def parse_css(self, response):
         matches = re.finditer('@import[^"\']*["\']([^"\']*)', response.body)
         # Download external CSS from a CSS file
@@ -48,25 +117,47 @@ class SimpleSpider(BaseSpider):
             abs_url = urlparse.urljoin(response.url, rel_url)
             yield Request(abs_url, callback=lambda _: None)
 
-    # Parses HTML pages for images, CSS, and javascript
+    """
+    Crawls content in a response
+
+    """
     def parse(self, response):
+        # Do not parse these mime types
+        skip_types = ['application/pdf', 'image/png', 'image/gif', 'image/jpeg']
+        if 'Content-Type' in response.headers:
+            if response.headers['Content-Type'] in skip_types:
+                return
+            if response.headers['Content-Type'].startswith("text/javascript"):
+                return
         # Get favicon
         yield Request(urlparse.urljoin(response.url, "/favicon.ico"),
                       callback=lambda _: None)
 
         hxs = HtmlXPathSelector(response)
+
+        # Parses anchor tags to find links to crawl
+        anchors = hxs.select('//a/@href').extract()
+        for rel_url in anchors:
+            abs_url = urlparse.urljoin(response.url, rel_url.strip())
+            if self.crawl_ruled(abs_url,
+                                accept_netlocs=self.accept_netlocs,
+                                regs_accept=self.regs_accept,
+                                regs_reject=self.regs_reject):
+                yield Request(abs_url)
+
+        # Parses for images
         image_tags = hxs.select('//img/@src').extract()
         for rel_url in image_tags:
             abs_url = urlparse.urljoin(response.url, rel_url.strip())
             yield Request(abs_url, callback=lambda _: None)
 
-        # Parse CSS
+        # Parses for CSS
         css_urls = hxs.select('//link[contains(@type,"css")]/@href').extract()
         for rel_url in css_urls:
             abs_url = urlparse.urljoin(response.url, rel_url.strip())
             yield Request(abs_url, callback=self.parse_css)
 
-        # Parse JavaScript
+        # Parses for JavaScript
         scripts = hxs.select('//script/@src').extract()
         for rel_url in scripts:
             abs_url = urlparse.urljoin(response.url, rel_url.strip())
